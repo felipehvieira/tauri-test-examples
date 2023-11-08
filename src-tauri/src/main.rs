ef82a55b6496464930bae4ecd8080d50fabe7ccb
Felipe Henriques <lipeh.pk@gmail.com>
2023-11-08 19:31:41 -0300

    fix windows configuration

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "linux")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use std::{process::{Command, Stdio}, io::{BufReader, BufRead}, os::windows::process::CommandExt};
 
 use tauri::{Manager, AppHandle};
